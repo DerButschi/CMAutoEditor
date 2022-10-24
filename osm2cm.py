b5c2f247a66198593f46fb0969f840fccec71960
Nico Möser <nicomoeser@gmx.net>
2022-10-24 20:33:06 +0200

    Changed license from Apache 2.0 to GPLv3

--- osm2cm.py
+++ osm2cm.py
@@ -1,16 +1,17 @@
-#    Copyright 2022 Nicolas Möser
+# Copyright (C) 2022  Nicolas Möser
 
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-#        http://www.apache.org/licenses/LICENSE-2.0
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License. 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from unicodedata import category
 from OSMPythonTools.overpass import Overpass, overpassQueryBuilder
@@ -1011,6 +1012,8 @@ def road_navigation_graph(df, gdf, name, cm_types):
         
         valid_paths = []
         a = 1
+
+    df = df.drop(df.loc[(df.name == name) & (df.direction == -1)].index)
                         
 
 
