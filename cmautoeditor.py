03682d5aa78510cee78855d97bd94f389864a8d3
Archie Hill <archiedhill@gmail.com>
2023-01-05 16:28:18 +0000

    Changed submit button to standard button as submit button no longer needed

--- cmautoeditor.py
+++ cmautoeditor.py
@@ -124,7 +124,7 @@ def display_gui():
         [sg.Input(), sg.FileBrowse(key='filepath', file_types=(('CSV files', '*.csv'),))],
         [sg.Text('Countdown: '), sg.InputCombo(key='countdown',values=[5, 10, 15, 20, 25, 30], default_value=10)],
         [sg.Text(text='', key='error_text')],
-        [sg.Push(), sg.Submit('Start CMAutoEditor', key='start'), sg.Exit(), sg.Push()]]
+        [sg.Push(), sg.Button('Start CMAutoEditor', key='start'), sg.Exit(), sg.Push()]]
 
     # Create window with layout
     window = sg.Window('CMAutoEditor', layout)
