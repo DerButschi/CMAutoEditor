b5014688efdb1c14e57a64324f10afa0e25148da
Nico Möser <nicomoeser@gmx.net>
2023-01-21 19:16:39 +0100

    improved path search for roads

--- osm_utils/path_search.py
+++ osm_utils/path_search.py
@@ -40,13 +40,193 @@ road_direction_dict = {
 
 direction_dict = dict((v,k) for k,v in road_direction_dict.items())
 
-def custom_weight(graph, node1, node2, edge_dict, ref_line):
-    cost1 = ref_line.interpolate(ref_line.project(Point(node1))).distance(Point(node1))
-    cost2 = ref_line.interpolate(ref_line.project(Point(node2))).distance(Point(node2))
+def custom_weight(graph: nx.Graph, node1, node2, edge_dict, ref_line, tiles, source, target):
+    # calculates the cost to go from current node (node1) to neighbor node (node2)
+    # Caculate diff in squares between node1 and node2, obtain "direction" for that
+    diff_node1_node2 = (node2[0] - node1[0], node2[1] - node1[1])
+    diff_node2_node1 = (node1[0] - node2[0], node1[1] - node2[1])
+    direction_node1 = road_direction_dict[diff_node1_node2]
+    direction_node2 = road_direction_dict[diff_node2_node1]
+    # check if either direction for either node1 or node2 is not in tile set.
+    if direction_node1 not in tiles.columns or direction_node2 not in tiles.columns:
+        return None
+    # check if direction of node1 is already taken
+    if 'connections' in graph.nodes[node1] and direction_node1 in graph.nodes[node1]['connections']:
+        return None
+    # check if direction of node1 is already taken
+    if 'connections' in graph.nodes[node2] and direction_node2 in graph.nodes[node2]['connections']:
+        return None
+    # now check if there is a tile in the tile set that has all the necessary connections
+    if 'connections' in graph.nodes[node1] and len(graph.nodes[node1]['connections']) > 0:
+        # only source and target node may have more than two connections
+        if len(graph.nodes[node1]['connections']) > 1 and not (node1 == source or node1 == target):
+            return None
+        condition = ~pandas.isnull(tiles[direction_node1])
+        for con in graph.nodes[node1]['connections']:
+            condition = condition & ~pandas.isnull(tiles[con])
+
+        if not condition.any():
+            return None 
+    if 'connections' in graph.nodes[node2] and len(graph.nodes[node2]['connections']) > 0:
+        # only source and target node may have more than two connections
+        if len(graph.nodes[node2]['connections']) > 1 and not (node2 == source or node2 == target):
+            return None
+        condition = ~pandas.isnull(tiles[direction_node2])
+        for con in graph.nodes[node2]['connections']:
+            condition = condition & ~pandas.isnull(tiles[con])
+
+        if not condition.any():
+            return None 
+
+    # create point object for node and store it so that this operation has to be done only once per node
+    if 'point' not in graph.nodes[node1]:
+        graph.nodes[node1]['point'] = Point(node1)
+    if 'point' not in graph.nodes[node2]:
+        graph.nodes[node2]['point'] = Point(node2)
+
+    point_node1 = graph.nodes[node1]['point']
+    point_node2 = graph.nodes[node2]['point']
+
+    # if all the checks above worked (i.e. there is a valid connection between both nodes),
+    # calculate the cost of this connection as the sum of distances of the nodes to the actual LineString
+    # This should minimize the distance of the path chosen to the LineString
+    cost1 = ref_line.interpolate(ref_line.project(point_node1)).distance(point_node1)
+    cost2 = ref_line.interpolate(ref_line.project(point_node2)).distance(point_node2)
     return cost1 + cost2
 
-# def search_path(line_graph, grid_gdf, df):
 def search_path(osm_processor, config, name):
+    logger = logging.getLogger('osm2cm')
+
+    if 'road_tiles' in config[name]['process']:
+        tiles = road_tiles
+    elif 'stream_tiles' in config[name]['process']:
+        tiles = stream_tiles
+    elif 'rail_tiles' in config[name]['process']:
+        tiles = rail_tiles
+    elif 'fence_tiles' in config[name]['process']:
+        tiles = fence_tiles
+
+    line_graph = osm_processor.network_graphs[name]['line_graph']
+    grid_gdf = osm_processor.gdf
+    df = osm_processor.df 
+
+    square_graph = nx.MultiGraph()
+
+    # TODO: Check what happens if point can't be placed.
+    # create grid graph
+    xmax = grid_gdf.xidx.max()
+    ymax = grid_gdf.yidx.max()
+
+    grid_graph = nx.grid_2d_graph(xmax+1, ymax+1)
+    if 'ul' in tiles.columns or 'ur' in tiles.columns or 'dl' in tiles.columns or 'dr' in tiles.columns:
+        diagonal_edges1 = [((x, y), (x+1, y+1)) for x in range(xmax) for y in range(ymax)]
+        diagonal_edges2 = [((x+1, y), (x, y+1)) for x in range(xmax) for y in range(ymax)]
+
+        grid_graph.add_edges_from(diagonal_edges1, weight=np.sqrt(2))
+        grid_graph.add_edges_from(diagonal_edges2, weight=np.sqrt(2))
+
+    edge_list = list(line_graph.edges)
+    edges_to_process = list(range(len(edge_list)))
+    path_dict = {}
+    plt.figure()
+    plt.axis('equal')
+    while len(edges_to_process) > 0:
+        edge_idx = edges_to_process.pop()
+        edge = edge_list[edge_idx]
+        # count += 1
+        # if count > 5:
+        #     break
+        closest_node_to_start = _get_closest_node_in_gdf(grid_gdf, edge[0])
+        closest_node_to_end = _get_closest_node_in_gdf(grid_gdf, edge[1])
+
+        edge_data = line_graph.get_edge_data(*edge)
+        ls = edge_data['ls']
+        ls_valid = []
+        for coord in ls.coords:
+            p_grid_closest = _get_closest_node_in_gdf(grid_gdf, coord)
+            # if p_grid_closest not in ls_valid:
+            #     ls_valid.append(p_grid_closest)
+            if len(ls_valid) == 0 or (len(ls_valid) > 0 and p_grid_closest != ls_valid[-1]):
+                ls_valid.append(p_grid_closest)
+        
+        if len(ls_valid) < 2:
+            continue
+        ls_valid = LineString(ls_valid)
+
+        try:
+            path = custom_weight_astar_path(grid_graph, closest_node_to_start, closest_node_to_end, weight=custom_weight, ref=ls_valid, tiles=tiles)
+        except nx.NetworkXNoPath:
+            logger.debug('No path found from {} to {}.'.format(edge[0], edge[1]))
+            continue
+
+        # check if path crosses the path of another edge at node that is not the start or end node
+        crosses = False
+        for node_idx in range(1, len(path)-1):
+            node = path[node_idx]
+            node_dict = grid_graph.nodes[node]
+            if 'connections' in node_dict and len(node_dict['connections']) > 2:
+                crosses = True
+                edges_to_process.append(edge_idx)
+                other_edge_idx = node_dict['edges'][-1]
+                edges_to_process.append(other_edge_idx)
+
+                logger.debug('Edge {} crosses edge {} reprocessing both.'.format(edge_idx, other_edge_idx))
+
+                # remove the other edge from the grid_graph
+                other_path = path_dict[other_edge_idx]
+                for other_nodes in other_path[0]:
+                    indices_to_remove = []
+                    for lidx, eidx in enumerate(grid_graph.nodes[other_nodes]['edges']):
+                        if eidx == other_edge_idx:
+                            indices_to_remove.append(lidx)
+                
+                    for lidx in indices_to_remove[::-1]:
+                        grid_graph.nodes[other_nodes]['edges'].pop(lidx)
+                        grid_graph.nodes[other_nodes]['connections'].pop(lidx)
+
+        if crosses:
+            continue
+
+        for nidx, node in enumerate(path):
+            if nidx < len(path) - 1:
+                next_node = path[nidx + 1]
+                diff = (next_node[0] - node[0], next_node[1] - node[1])
+                direction = road_direction_dict[diff]
+                if not 'connections' in grid_graph.nodes[path[nidx]]:
+                    grid_graph.nodes[path[nidx]]['connections'] = []
+                    grid_graph.nodes[path[nidx]]['edges'] = []
+                grid_graph.nodes[path[nidx]]['connections'].append(direction)
+                grid_graph.nodes[path[nidx]]['edges'].append(edge_idx)
+            if nidx > 0:
+                prev_node = path[nidx - 1]
+                diff = (prev_node[0] - node[0], prev_node[1] - node[1])
+                direction = road_direction_dict[diff]
+                if not 'connections' in grid_graph.nodes[path[nidx]]:
+                    grid_graph.nodes[path[nidx]]['connections'] = []
+                    grid_graph.nodes[path[nidx]]['edges'] = []
+                grid_graph.nodes[path[nidx]]['connections'].append(direction)
+                grid_graph.nodes[path[nidx]]['edges'].append(edge_idx)
+
+        plt.plot(ls.xy[0], ls.xy[1], '-ko')
+        square_geom = [grid_gdf[(grid_gdf.xidx == p[0]) & (grid_gdf.yidx == p[1])].geometry.values[0] for p in path]
+        for g in square_geom:
+            plt.plot(g.exterior.xy[0], g.exterior.xy[1], '-b')
+
+        path_dict[edge_idx] = [path, edge_data['element_idx']]
+
+    plt.show()
+    
+    for edge_idx in path_dict.keys():
+        path = path_dict[edge_idx][0]
+        edge_data = path_dict[edge_idx][1]
+
+        square_graph.add_edge(path[0], path[-1], squares=path, element_idx=edge_data, from_node_to_node=[path[0], path[-1]])
+
+    osm_processor.network_graphs[name]['square_graph'] = square_graph
+
+    
+# def search_path(line_graph, grid_gdf, df):
+def search_path2(osm_processor, config, name):
     logger = logging.getLogger('osm2cm')
 
     if 'road_tiles' in config[name]['process']:
@@ -240,7 +420,7 @@ def _get_invalid_nodes_around_node(node_xy, node_connections_dict, gdf, tiles):
 
     return invalid_nodes
 
-def custom_weight_astar_path(G, source, target, heuristic=None, weight="weight", ref=None):
+def custom_weight_astar_path(G, source, target, heuristic=None, weight="weight", ref=None, tiles=None):
     """Returns a list of nodes in a shortest path between source and target
     using the A* ("A-star") algorithm.
 
@@ -366,7 +546,7 @@ def custom_weight_astar_path(G, source, target, heuristic=None, weight="weight",
         explored[curnode] = parent
 
         for neighbor, w in G[curnode].items():
-            cost = weight(G, curnode, neighbor, w, ref)
+            cost = weight(G, curnode, neighbor, w, ref, tiles, source, target)
             if cost is None:
                 continue
             ncost = dist + cost
