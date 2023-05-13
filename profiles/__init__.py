1add1902b89f2073b916e65ce4d3ae8ed3ff066e
Nico Möser <nicomoeser@gmx.net>
2023-05-13 23:24:03 +0200

    added cmsf2 profile

--- profiles/__init__.py
+++ profiles/__init__.py
@@ -1,10 +1,12 @@
 from collections import OrderedDict
 from . import fortress_italy
 from . import cold_war
+from . import shock_force_2
 
 available_profiles = OrderedDict([
     ('Cold War', 'cold_war'),
-    ('Fortress Italy', 'fortress_italy')
+    ('Fortress Italy', 'fortress_italy'),
+    ('Shock Force 2', 'shock_force_2')
 ])
 
 def get_building_tiles(building_type, profile='cold_war'):
@@ -12,9 +14,13 @@ def get_building_tiles(building_type, profile='cold_war'):
         return cold_war.get_building_tiles(building_type)
     elif profile == 'fortress_italy':
         return fortress_italy.get_building_tiles(building_type)
+    elif profile == 'schock_force_2':
+        return shock_force_2.get_building_tiles(building_type)
 
 def get_building_cat2(building_type, row, col, profile='cold_war'):
     if profile == 'cold_war':
         return cold_war.get_building_cat2(building_type, row, col)
     elif profile == 'fortress_italy':
         return fortress_italy.get_building_cat2(building_type, row, col)
+    elif profile == 'schock_force_2':
+        return shock_force_2.get_building_cat2(building_type, row, col)
